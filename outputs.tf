ee113f297db2e1a1e81778afc9b6bab57263a7c5
Iulian Bacalu <ibacalu@icloud.com>
2025-02-22 14:10:26 +0100

    fix: terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "this" {
 output "info" {
   description = "LXC Info"
   value = {
-    id = try(split("/", proxmox_lxc.this.id)[2], "")
+    id   = try(split("/", proxmox_lxc.this.id)[2], "")
     host = local.target_node
   }
 }
 
 output "network" {
   description = "Network configuration"
-  value = try(proxmox_lxc.this.network, {})
+  value       = try(proxmox_lxc.this.network, {})
 }
